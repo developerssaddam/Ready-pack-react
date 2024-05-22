c1bce1f3aa17a630e99d0da09d610b442a347f73
developersaddam <developerssaddam@gmail.com>
2024-05-22 15:40:43 +0600

    Added a env.local file and setup main layout

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 
